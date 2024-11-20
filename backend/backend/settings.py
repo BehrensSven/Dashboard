ef461611e127516d86665ed416369397c6bfc6f7
Sven_Behrens <sven.behrens@securepoint.de>
2024-11-20 10:15:38 +0100

    fix: The token only expires after one hour

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -143,3 +143,13 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
 }
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': True,
+}
