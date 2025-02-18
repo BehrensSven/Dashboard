4ab256f6bef268cfd22a5a9a6267c51a969124ab
Sven_Behrens <sven.behrens@securepoint.de>
2025-02-18 16:43:30 +0100

    Automatic landing of prefabricated data

---
+++ backend/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# Führe Datenbankmigrationen durch
+python manage.py migrate --noinput
+
+# Lade die vorgefertigten Daten
+python manage.py loaddata fixtures/initial_data.json
+
+# Starte den Django-Entwicklungsserver
+exec python manage.py runserver 0.0.0.0:8000
